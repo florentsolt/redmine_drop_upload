6fe8e7b15029223ca3fa8bce356ae4ddf15e8774
Sys <sys@netvibes.com>
2015-05-13 17:06:29 +0200

    Fix routes for Redmine3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-match '/uploads', :to => 'uploads#index'
-match '/uploads/file/:filename', :to => 'uploads#file', :filename => /.*/
-match '/uploads/upload', :to => 'uploads#upload'
-match '/uploads/delete/:filename', :to => 'uploads#delete', :filename => /.*/
+match '/uploads', :to => 'uploads#index', :via => :get
+match '/uploads/file/:filename', :to => 'uploads#file', :filename => /.*/, :via => :get
+match '/uploads/upload', :to => 'uploads#upload', :via => :post
+match '/uploads/delete/:filename', :to => 'uploads#delete', :filename => /.*/, :via => :get
